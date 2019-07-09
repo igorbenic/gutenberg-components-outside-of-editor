d0f14e6d6c3ccc416ea29bd5b8f82ef3bf1178da
Igor Benić <i.benic@hotmail.com>
2019-07-10 00:51:03 +0200

    Updates

---
+++ precache-manifest.543255617f80072138d8bf3fd0e416e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1712056474f749630e540e179a18da9b",
+    "url": "/gutenberg-components-outside-of-editor/index.html"
+  },
+  {
+    "revision": "1bd62f3ebf187890872c",
+    "url": "/gutenberg-components-outside-of-editor/static/css/2.256e20f1.chunk.css"
+  },
+  {
+    "revision": "980aa24d1d89d71699ce",
+    "url": "/gutenberg-components-outside-of-editor/static/css/main.40b3eaac.chunk.css"
+  },
+  {
+    "revision": "1bd62f3ebf187890872c",
+    "url": "/gutenberg-components-outside-of-editor/static/js/2.035b7068.chunk.js"
+  },
+  {
+    "revision": "980aa24d1d89d71699ce",
+    "url": "/gutenberg-components-outside-of-editor/static/js/main.99cf8f3e.chunk.js"
+  },
+  {
+    "revision": "a3cf32c7a46e777e8fde",
+    "url": "/gutenberg-components-outside-of-editor/static/js/runtime~main.a3f4783d.js"
+  },
+  {
+    "revision": "f91b5c1ee526f5610399512df5943cf2",
+    "url": "/gutenberg-components-outside-of-editor/static/media/logo.f91b5c1e.png"
+  }
+]);
